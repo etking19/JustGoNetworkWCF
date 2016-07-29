b9958faf716decaf3535d062b1f2bc0be6aa549a
etking19 <etking0072004@hotmail.com>
2016-07-29 15:55:04 +0800

    - added basic authentication method

---
+++ WcfService/WcfService/BasicAuthHttpModule.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace WcfService
+{
+    public class BasicAuthHttpModule : IHttpModule
+    {
+        private const string Realm = "My Realm";
+        private static string[] _users;
+        private static string[] _tokens;
+
+        public void Init(HttpApplication context)
+        {
+            // Register event handlers
+            context.AuthenticateRequest += OnApplicationAuthenticateRequest;
+            context.EndRequest += OnApplicationEndRequest;
+
+            // get all the username and tokens from web.config
+            _users = ConfigurationManager.AppSettings["AuthorizationUser"].Split(';');
+            _tokens = ConfigurationManager.AppSettings["AuthorizationToken"].Split(';');
+        }
+
+        private static void SetPrincipal(IPrincipal principal)
+        {
+            Thread.CurrentPrincipal = principal;
+            if (HttpContext.Current != null)
+            {
+                HttpContext.Current.User = principal;
+            }
+        }
+
+        private static bool CheckPassword(string username, string password)
+        {
+            for (int count = 0; count < _users.Length; count++)
+            {
+                if(_users[count] == username &&
+                    _tokens[count] == password)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void AuthenticateUser(string credentials)
+        {
+            try
+            {
+                var encoding = Encoding.GetEncoding("iso-8859-1");
+                credentials = encoding.GetString(Convert.FromBase64String(credentials));
+
+                int separator = credentials.IndexOf(':');
+                string name = credentials.Substring(0, separator);
+                string password = credentials.Substring(separator + 1);
+
+                if (CheckPassword(name, password))
+                {
+                    var identity = new GenericIdentity(name);
+                    SetPrincipal(new GenericPrincipal(identity, null));
+                }
+                else
+                {
+                    // Invalid username or password.
+                    HttpContext.Current.Response.StatusCode = 401;
+                }
+            }
+            catch (Exception e)
+            {
+                // Credentials were not formatted correctly.
+                Console.WriteLine(e.Message);
+                HttpContext.Current.Response.StatusCode = 401;
+            }
+        }
+
+        private static void OnApplicationAuthenticateRequest(object sender, EventArgs e)
+        {
+            var request = HttpContext.Current.Request;
+            var authHeader = request.Headers["Authorization"];
+            if (authHeader != null)
+            {
+                var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+
+                // RFC 2617 sec 1.2, "scheme" name is case-insensitive
+                if (authHeaderVal.Scheme.Equals("basic",
+                        StringComparison.OrdinalIgnoreCase) &&
+                    authHeaderVal.Parameter != null)
+                {
+                    AuthenticateUser(authHeaderVal.Parameter);
+                }
+            }
+        }
+
+        // If the request was unauthorized, add the WWW-Authenticate header 
+        // to the response.
+        private static void OnApplicationEndRequest(object sender, EventArgs e)
+        {
+            var response = HttpContext.Current.Response;
+            if (response.StatusCode == 401)
+            {
+                response.Headers.Add("WWW-Authenticate",
+                    string.Format("Basic realm=\"{0}\"", Realm));
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
